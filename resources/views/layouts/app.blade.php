7dca3cd2b319c9078ca2e585a4c0621861bc82c9
Luis Henrique Mildenberg <hard.henrique@outlook.com>
2022-05-16 08:58:45 -0300

    alteração de nomenclatura

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,13 +61,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Entre') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Cadastre-se') }}</a>
                                 </li>
                             @endif
                         @else
